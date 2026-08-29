192b0849b287a0f1185cfef97221cb0fe66b1f5a
bexo5004 <bexo5004@gmail.com>
2026-08-30 01:58:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -318,7 +318,7 @@ async def receive_files(update: Update, context: ContextTypes.DEFAULT_TYPE):
             
         except Exception as e:
             logger.error(f"خطأ: {e}")
-            await update.message.reply_text("❌ حدث خطأ في تحميل الملف")
+            await update.message.reply_text(" حدث خطأ في تحميل الملف بسب حجم الملفات كبيره رجاء ارسال الملفات الكبيره الى @bexo0 مع اسم الملف❤️🌿 ")
     
     return WAIT_FILE
 
